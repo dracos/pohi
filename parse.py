63a1a08506b25083389c24a20cfd01f70f6b2865
Matthew Somerville <matthew@mysociety.org>
2023-05-09 10:38:06 +0100

    Spot Further submission heading.

--- parse.py
+++ parse.py
@@ -220,7 +220,7 @@ def parse_transcript(url, text):
             continue
 
         # Headings
-        m = re.match('Focus Group Session [34]$|Housekeeping$|Opening statement by [A-Z ]*(?:, QC)?(?: \(continued\))?$|[A-Z ]*$|Announcements by [A-Z ]*$|Submissions? by [A-Z ]*(?:, QC)?$', line.strip())
+        m = re.match('Focus Group Session [34]$|Housekeeping$|Opening statement by [A-Z ]*(?:, QC)?(?: \(continued\))?$|[A-Z ]*$|Announcements by [A-Z ]*$|(Further s|S)ubmissions? by [A-Z ]*(?:, QC)?$', line.strip())
         if m:
             yield speech
             speech = Section( heading=string.capwords(line.strip()).replace('Qc', 'QC') )
