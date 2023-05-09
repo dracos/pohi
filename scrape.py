311d49d04ffe720ae26beff48e84138875270c16
Matthew Somerville <matthew@mysociety.org>
2023-05-09 23:05:42 +0100

    Add GitHub action to scrape/parse.

--- scrape.py
+++ scrape.py
@@ -1,6 +1,7 @@
 import bs4
 import datetime
 import json
+import os
 import re
 import requests
 import requests_cache
@@ -27,7 +28,8 @@ def fetch_hearing_page(item):
     link = item.h2.a['href']
     title = item.h2.text  # e.g. Phase 2 - 25 November 2022
     date = datetime.datetime.strptime(item.time.text, '%d %B %Y').date()  # e.g. 25 November 2022
-    desc = item.find(class_='hearing-item-body').text
+    if os.path.exists(f'data/{date}-{title}.txt'):
+        return
     url = urllib.parse.urljoin(BASE, link)
     r = session.get(url)
     soup = bs4.BeautifulSoup(r.content, "html.parser")
@@ -42,11 +44,17 @@ def fetch_hearing_page(item):
             content = re.sub(b'\n\n', b'\n', content)
             fp.write(content)
 
+EVIDENCE = {}
+def load_evidence():
+    if os.path.exists('data/evidence.json'):
+        evidence = json.load(open('data/evidence.json'))
+        for url, href, note in evidence:
+            EVIDENCE.setdefault(url, []).append((url, href, note))
+
 def fetch_evidence():
     url = BASE_EVI
     evidence = []
     while True:
-        print(url)
         r = requests.get(url)
         soup = bs4.BeautifulSoup(r.content, "html.parser")
         for item in soup.find_all('div', class_='evidence-item-wrapper'):
@@ -60,15 +68,18 @@ def fetch_evidence():
 def fetch_evidence_page(item):
     link = item.h2.a['href']
     url = urllib.parse.urljoin(BASE_EVI, link)
+    if url in EVIDENCE:
+        return EVIDENCE[url]
     r = session.get(url)
     soup = bs4.BeautifulSoup(r.content, "html.parser")
     data = []
     for link in soup.find_all('span', class_='file'):
         href = urllib.parse.urljoin(BASE_EVI, link.a['href'])
         note = link.a.text
-        print(' ', href, note)
+        print(url, note)
         data.append((url, href, note))
     return data
 
 fetch_hearings()
+load_evidence()
 fetch_evidence()
