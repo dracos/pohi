2a796b92d0ef60097482ad0e687fe68c679f9470
Matthew Somerville <matthew@mysociety.org>
2024-07-16 20:53:27 +0100

    [Meta] Apparently need to set this manually now.
    See https://about.readthedocs.com/blog/2024/07/addons-by-default/

--- conf.py
+++ conf.py
@@ -1,5 +1,16 @@
 # Configuration file for the Sphinx documentation builder.
 
+import os
+
+# Define the canonical URL if you are using a custom domain on Read the Docs
+html_baseurl = os.environ.get("READTHEDOCS_CANONICAL_URL", "")
+
+# Tell Jinja2 templates the build is running on Read the Docs
+if os.environ.get("READTHEDOCS", "") == "True":
+    if "html_context" not in globals():
+        html_context = {}
+    html_context["READTHEDOCS"] = True
+
 # -- Project information
 
 project = 'Searchable transcripts of the Post Office Horizon IT Inquiry hearings'
